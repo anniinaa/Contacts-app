d3d269441507a8803148d33c94a97baf46b7c40a
Anniina Aarnio <aarnioanniina@gmail.com>
2021-07-20 13:03:36 +0300

    add routing

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,15 @@
-function Details() {
+function Details(props) {
+  const id = props.match.params.id;
+
   return (
     <div className="details">
-      <p>MOIKKA</p>
+      <div className="card">
+        <h4 className="name">Anniina Aarnio {id}</h4>
+        <p className="address">Lohkopellontie 36</p>
+        <p className="zip">444442</p>
+        <p className="city">Helsinki</p>
+        <p className="phoneNumber">+358 456 524456</p>
+      </div>
     </div>
   );
 }
